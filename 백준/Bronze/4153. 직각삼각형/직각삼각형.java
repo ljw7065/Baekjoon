8a1325809cbc7273655252a001c1c705fbb6667c
ljw7065 <151030486+ljw7065@users.noreply.github.com>
2023-11-24 00:25:33 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,29 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (sc.hasNext()) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();			
-			
-			if(a==0 && b==0 && c==0) {
+		while (true) {
+			String[] str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[1]);
+			int c = Integer.parseInt(str[2]);
+
+			if (a==0 && b==0 && c==0) {
 				break;
 			}
-			
-			if((a*a)==(b*b)+(c*c) || (b*b)==(a*a)+(c*c) || (c*c)==(a*a)+(b*b)) {
+
+			if ((a*a)==(b*b)+(c*c) || (b*b)==(a*a)+(c*c) || (c*c)==(a*a)+(b*b)) {
 				System.out.println("right");
 			} else {
 				System.out.println("wrong");
 			}
-			
-			
+
+			bw.newLine();
+
 		}
+
+		bw.flush();
+		br.close();
 
 	}
 
 }
-
